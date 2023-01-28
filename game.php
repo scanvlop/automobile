02644f17e6aba0e9e16e06468d93c745ac38e966
scanvlop <99763267+scanvlop@users.noreply.github.com>
2023-01-28 11:38:33 +0530

    Update game.php

--- game.php
+++ game.php
@@ -1,89 +1,45 @@
-<?php
+<?php // Do not put any HTML above this line
+session_start();
 
-// Demand a GET parameter
-if ( ! isset($_GET['name']) || strlen($_GET['name']) < 1  ) {
-    die('Name parameter missing');
+require_once "pdo.php";
+
+if ( ! isset($_SESSION['name']) ) {
+    die('Not logged in');
 }
 
-// If the user requested logout go back to index.php
-if ( isset($_POST['logout']) ) {
-    header('Location: index.php');
-    return;
-}
-
-// Set up the values for the game...
-// 0 is Rock, 1 is Paper, and 2 is Scissors
-$names = array('Rock', 'Paper', 'Scissors');
-$human = isset($_POST["human"]) ? $_POST['human']+0 : -1;
-
-$computer = 0; // Hard code the computer to rock
-// TODO: Make the computer be random
-// $computer = rand(0,2);
-
-// This function takes as its input the computer and human play
-// and returns "Tie", "You Lose", "You Win" depending on play
-// where "You" is the human being addressed by the computer
-function check($computer, $human) {
-    // For now this is a rock-savant checking function
-    // TODO: Fix this
-    if ( $human == 0 ) {
-        return "Tie";
-    } else if ( $human == 1 ) {
-        return "You Win";
-    } else if ( $human == 2 ) {
-        return "You Lose";
-    }
-    return false;
-}
-
-// Check to see how the play happenned
-$result = check($computer, $human);
-
+$stmt = $pdo->query("SELECT make, year, mileage FROM autos");
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Dr. Chuck's Rock, Paper, Scissors Game</title>
-<?php require_once "bootstrap.php"; ?>
+    <?php require_once "bootstrap.php"; ?>
+    <title>Login Page</title>
 </head>
 <body>
 <div class="container">
-<h1>Rock Paper Scissors</h1>
-<?php
-if ( isset($_REQUEST['name']) ) {
-    echo "<p>Welcome: ";
-    echo htmlentities($_REQUEST['name']);
-    echo "</p>\n";
-}
-?>
-<form method="post">
-<select name="human">
-<option value="-1">Select</option>
-<option value="0">Rock</option>
-<option value="1">Paper</option>
-<option value="2">Scissors</option>
-<option value="3">Test</option>
-</select>
-<input type="submit" value="Play">
-<input type="submit" name="logout" value="Logout">
-</form>
-
-<pre>
-<?php
-if ( $human == -1 ) {
-    print "Please select a strategy and press Play.\n";
-} else if ( $human == 3 ) {
-    for($c=0;$c<3;$c++) {
-        for($h=0;$h<3;$h++) {
-            $r = check($c, $h);
-            print "Human=$names[$h] Computer=$names[$c] Result=$r\n";
-        }
+    <h1>Tracking Autos for <?php echo $_SESSION['name']; ?></h1>
+    <?php
+    if ( isset($_SESSION['success']) ) {
+    echo('<p style="color: green;">'.htmlentities($_SESSION['success'])."</p>\n");
+    unset($_SESSION['success']);
     }
-} else {
-    print "Your Play=$names[$human] Computer Play=$names[$computer] Result=$result\n";
-}
-?>
-</pre>
+    ?>
+    <h2>Automobiles</h2>
+    <ul>
+
+        <?php
+        foreach ($rows as $row) {
+            echo '<li>';
+            echo htmlentities($row['make']) . ' ' . $row['year'] . ' / ' . $row['mileage'];
+        };
+        echo '</li><br/>';
+        ?>
+    </ul>
+    <p>
+        <a href="add.php">Add New</a> |
+        <a href="logout.php">Logout</a>
+    </p>
 </div>
 </body>
 </html>
